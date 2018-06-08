7056471ef52ef93f51ca648bc9f4b1de8f7ac3df
Ing. Ondrej ĎURÁS (dury) <ondrej-duras@users.noreply.github.com>
2018-06-08 11:30:36 +0200

    --line --no-line --name --no-name added to command-line parameters

--- craw.pl
+++ craw.pl
@@ -4,7 +4,7 @@
 use strict;
 use warnings;
 
-our $VERSION = 2017.113001;
+our $VERSION = 2018.060801;
 our $MANUAL  = <<__MANUAL__;
 NAME: CRAW - cisco config parser
 FILE: craw.pl
@@ -18,13 +18,19 @@ USAGE:
   craw --secion "description Enclosure" --file ROUTER-conf.txt
   craw --interface gi1/0/23   --file ROUTER-conf.txt
   craw --snmp   --file ROUTER-conf.txt
+  craw --line --parent "23[89]\\.[0-9]+\\.[0-9]\\." -f CONFIG.txt
+  find . -type f -exec craw -name -line -parent 213\\.151\\. --file % \\;
 
 PARAMETERS:
   --file <File>   - configuration file
   --parent <RegEx> - searches for RegEx, writes all parent details
   --section <RegEx> - searches for RegEx, writes whole section
   --interface <ifName> - searches for interface config
-  --snmp  - provides all interesting SNMP configuration details
+  --snmp    - provides all interesting SNMP configuration details
+  --line    - shows the line numbers
+  --no-line - suppress the line numbers
+  --name    - shows the filename
+  --no-name - suppress the filename
 
 SEE ALSO:
   https://github.com/ondrej-duras/
@@ -41,10 +47,13 @@ our $INDENT = 0;
 our $FFLAG  = 0;
 our $TEXT   = 0;
 our $STACK  = [];
-our $MODE_PARENT    = 0; # mode to find all parent settings related to the match
-our $MODE_SECTION   = 0; # mode to find whole section related to the match
-our $MODE_INTERFACE = 0; # mode to find configuration of interface, focused on interface name translations
-our $MODE_SNMP      = 0; # mode to finde all snmp settings
+our $MODE_PARENT    = 0;   # mode to find all parent settings related to the match
+our $MODE_SECTION   = 0;   # mode to find whole section related to the match
+our $MODE_INTERFACE = 0;   # mode to find configuration of interface, focused on interface name translations
+our $MODE_SNMP      = 0;   # mode to finde all snmp settings
+our $MODE_LINE      = 0;   # mode to display line numbers in front of text on output  (--line / --no-line)
+our $MODE_NAME      = 0;   # mode to display a source FileName in front of text output (--name/ --no-name)
+our $MODE_DELIMITER = ";"; # string used as the delimiter for output
 our @APARAMS = (); # List of all RegularExpressions/Interfaces/...
 =pod
 1. Subor je spracovavany poriadkoch
@@ -71,10 +80,15 @@ unless(scalar @ARGV) {
 
 while(my $ARGX = shift @ARGV) {
   if($ARGX =~ /^-+snmp/) { $MODE_SNMP = 1; next; }  # --snmp
-  if($ARGX =~ /^-+f/) { $FNAME = shift @ARGV; next; }  # --file <FileName>
-  if($ARGX =~ /^-+p/) { $MODE_PARENT    = 1; my $X=shift @ARGV; push @APARAMS,$X; next; }      # --parent
-  if($ARGX =~ /^-+s/) { $MODE_SECTION   = 1; my $X=shift @ARGV; push @APARAMS,$X; next; }      # --section
-  if($ARGX =~ /^-+i/) { $MODE_INTERFACE = 1; my $X=shift @ARGV; push @APARAMS,$X; next; }      # --interface
+  if($ARGX =~ /^-+f/)     { $FNAME = shift @ARGV; next; }  # --file <FileName>
+  if($ARGX =~ /^-+p/)     { $MODE_PARENT    = 1; my $X=shift @ARGV; push @APARAMS,$X; next; }      # --parent
+  if($ARGX =~ /^-+s/)     { $MODE_SECTION   = 1; my $X=shift @ARGV; push @APARAMS,$X; next; }      # --section
+  if($ARGX =~ /^-+i/)     { $MODE_INTERFACE = 1; my $X=shift @ARGV; push @APARAMS,$X; next; }      # --interface
+  if($ARGX =~ /^-+d/)     { $MODE_DELIMITER = shift @ARGV;  next; }      # --delimiter ";"
+  if($ARGX =~ /^-+li/)    { $MODE_LINE = 1; next; }      # --line
+  if($ARGX =~ /^-+no-?li/){ $MODE_LINE = 0; next; }      # --line
+  if($ARGX =~ /^-+na/)    { $MODE_NAME = 1; next; }      # --name
+  if($ARGX =~ /^-+no-?na/){ $MODE_NAME = 0; next; }      # --name
   push @APARAMS,$ARGX;
 }
 
@@ -83,9 +97,11 @@ if((not $FNAME) and ( not -t STDIN)) { $FNAME="-"; }
 if($MODE_PARENT) {
   if($FNAME eq "-") { open $FH,"<&STDIN" or die "#- Error: somwthing worng with STDIN !\n"; }
   else { open $FH,"<",$FNAME or die "#- Error: unreachable file '${FNAME}' !\n"; }
+  my $CTLINE=0;
   while($LINE = <$FH>) {
 
     # customizing line
+    $CTLINE++;
     chomp $LINE;
     next if $LINE =~ /^\s*$/;
     next if $LINE =~ /^\s*!/;
@@ -105,10 +121,14 @@ if($MODE_PARENT) {
     }
 
     # pushing new line to stack
+    my $ZTEXT = "";
+    if($MODE_LINE) { $ZTEXT = sprintf("%7i",$CTLINE) . $MODE_DELIMITER . $ZTEXT; }
+    if($MODE_NAME) { $ZTEXT = $FNAME . $MODE_DELIMITER . $ZTEXT; }
     my $PT = [];
     $PT->[0] = $INDENT;
     $PT->[1] = 0;
     $PT->[2] = $TEXT;
+    $PT->[3] = $ZTEXT;
     unshift @$STACK,$PT;
 
     # checking whether the LINE matches one of paterns/parameters 
@@ -125,7 +145,8 @@ if($MODE_PARENT) {
         my $XTEXT = $PARAM->[2];
         my $INDENT= $PARAM->[0];
         my $YTEXT = substr("                  ",0,$INDENT);
-        print "${YTEXT}${XTEXT}\n";
+        my $ZTEXT = $PARAM->[3];
+        print "${ZTEXT}${YTEXT}${XTEXT}\n";
       }
     }
   }
