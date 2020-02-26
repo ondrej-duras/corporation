22a0617c1c22ec2ead5416f990271ec6eb15daed
Ing. Ondrej ĎURÁS (dury) <ondrej-duras@users.noreply.github.com>
2020-02-26 19:50:19 +0100

    improves content of main window label

--- tel.sh
+++ tel.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+ARGC=$#
 export DEVIP=$1
 export PUTTY=`echo $SSH_TTY|sed 's/[^0-9]//g'`
-printf "\033]0;[%s] %s\007" "${PUTTY}" "${DEVIP}"
-telnet ${DEVIP}
-
+if [ "${ARGC}" -eq "1" ]; then 
+  printf "\033]0;[%s] %s\007" "${PUTTY}" "${DEVIP}"
+  echo "telnet ${DEVIP}"
+  telnet ${DEVIP}
+elif [ "${ARGC}" -eq "2" ]; then
+  printf "\033]0;[%s] %s\007" "${PUTTY}" "${DEVIP}:${2}"
+  echo "telnet ${DEVIP} ${2}"
+  telnet ${DEVIP} $2
+else
+  echo "Error: provide hostname or hostname and port !"
+fi
 
